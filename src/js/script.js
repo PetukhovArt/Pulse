a8adbe0de24c9fee8f7f820c3148e7de5c52c331
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2022-11-15 18:32:01 +0500

    feat: add  sec slider + slick slider

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        dots: true,
+        adaptiveHeight: true,
+        });
+});
