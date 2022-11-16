9d12852e1dbb99e21ad54fd6d9e89208dc995aef
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2022-11-16 18:19:48 +0500

    fix: edit slider pref

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,18 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2500,
-        dots: true,
-        adaptiveHeight: true,
+        autoplaySpeed: 2000,
+        // adaptiveHeight: true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
         });
 });
